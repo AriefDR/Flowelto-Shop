a4d1251e9a13d5f813bed134d167b502bb791346
AriefDr <ariefdwir16@gmail.com>
2020-10-14 12:23:24 +0700

    change new flower photo in slide show

--- resources/views/managerView/homeManager.blade.php
+++ resources/views/managerView/homeManager.blade.php
@@ -34,71 +34,64 @@
     <div class="carousel-inner row w-100 mx-auto">
     <div class="carousel-item col-md-4 active">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/f44242/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga1.jpg')}}" alt="Bunga 1">
         <div class="card-body">
-            <h4 class="card-title">Card 1</h4>
+            <h4 class="card-title">Bunga 1</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
     <div class="carousel-item col-md-4">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/418cf4/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga2.jpg')}}" alt="Bunga 2">
         <div class="card-body">
-            <h4 class="card-title">Card 2</h4>
+            <h4 class="card-title">Bunga 2</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
     <div class="carousel-item col-md-4">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/3ed846/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga3.jpg')}}" alt="Bunga 3">
         <div class="card-body">
-            <h4 class="card-title">Card 3</h4>
+            <h4 class="card-title">Bunga 3</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
     <div class="carousel-item col-md-4">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/42ebf4/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga4.jpg')}}" alt="Bunga 4">
         <div class="card-body">
             <h4 class="card-title">Card 4</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
     <div class="carousel-item col-md-4">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/f49b41/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga5.jpg')}}" alt="Bunga 5">
         <div class="card-body">
             <h4 class="card-title">Card 5</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
     <div class="carousel-item col-md-4">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/f4f141/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga6.jpg')}}" alt="Bunga 6">
         <div class="card-body">
             <h4 class="card-title">Card 6</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
     <div class="carousel-item col-md-4">
         <div class="card">
-        <img class="card-img-top img-fluid" src="http://placehold.it/800x600/8e41f4/fff" alt="Card image cap">
+        <img class="card-img-top img-fluid" src="{{asset('/img/bunga7.jpg')}}" alt="Bunga 7">
         <div class="card-body">
             <h4 class="card-title">Card 7</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
         </div>
     </div>
