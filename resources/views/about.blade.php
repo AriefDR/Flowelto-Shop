a1bafb2236479675b11d15263b62844f95b6ddf8
Febio Irsadad <irsadad@Febios-MacBook-Pro.local>
2020-10-17 20:11:42 +0700

    message

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.main')
+
+@section('title', 'About')
+
+@section('container')
+<br><br><br><br>
+    <h4 class="text-center font-weight-bold m-4">About Us</h4>
+
+    <div class="container">
+        <div class="text-center">
+        <p>Flowelto Shop is a flower house located in West Jakarta, Kebon Jeruk. We've been serving you since 2020 and it feels amazing to be a "Messenger" to our beloved customers for the past years. Professionally curated by a local expert La Madame Florist, with more than hundreds of bouquet, flower box, flower basket and gift options to choose from, whatever may be the occasion: season’s greeting, anniversary, graduation wishes, birthday, valentines, wedding, engagement, newborn, Christmas, Eid Mubarak and many more categories that cover the entire canvas of life.</p>
+        <p>It is perhaps the most used and successful form of communicating your feelings to those you care about. At Flowelto Shop, we realize the worth of your emotions which is what makes us the number one choice for sending out your love to dear ones. Be it a happy occasion or a sad one. Flowelto Shop is just a click away.</p>
+        <p>A good life is a collection of happy moments. Let’s create your happy moments with us.</p>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br>
+@endsection
