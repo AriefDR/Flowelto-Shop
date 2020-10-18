d597ee124accc991cb9e6fc79748056f98be5d5a
AriefDr <ariefdwir16@gmail.com>
2020-10-18 10:16:41 +0700

    delete new flowers slideshow

--- public/js/homeManager.js
+++ public/js/homeManager.js
@@ -11,32 +11,4 @@ function showTime() {
     var bulanarray=new Array("January","February","March","April","May","June","July","August","September","October","Nopember","Desember");
     document.getElementById("datetime").innerHTML = hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun;
 }
-function carausel() {
-    $(document).ready(function() {
-        $("#myCarousel").on("slide.bs.carousel", function(e) {
-          var $e = $(e.relatedTarget);
-          var idx = $e.index();
-          var itemsPerSlide = 3;
-          var totalItems = $(".carousel-item").length;
-
-          if (idx >= totalItems - (itemsPerSlide - 1)) {
-            var it = itemsPerSlide - (totalItems - idx);
-            for (var i = 0; i < it; i++) {
-              // append slides to end
-              if (e.direction == "left") {
-                $(".carousel-item")
-                  .eq(i)
-                  .appendTo(".carousel-inner");
-              } else {
-                $(".carousel-item")
-                  .eq(0)
-                  .appendTo($(this).find(".carousel-inner"));
-              }
-            }
-          }
-        });
-      });
-
-}
 showTime()
-carausel()
